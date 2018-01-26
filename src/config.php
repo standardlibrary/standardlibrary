e0b9d1c3fbcd1070ce07ab584a80dc8a32c402fc
unwarysheep <unwarysheep@icloud.com>
2018-01-26 16:12:55 +0000

    Add settings for master

--- src/config.php
+++ src/config.php
@@ -11,11 +11,19 @@ $iterator = Finder::create()
     ->in($dir = 'vendor/standardlibrary/')
 ;
 
+// generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
+$versions = GitVersionCollection::create($dir)
+    //->addFromTags('v1.0.*')
+    //->add('1.0', '1.0 branch')
+    ->add('master', 'master branch')
+;
+
 return new Sami($iterator, array(
     'theme'                => 'main',
     'title'                => 'Standard Library',
-    'build_dir'            => 'docs',
-    // use a custom theme directory (this works if you install your dependencies in vendor/ folder)
+    'versions'             => $versions,
+    'build_dir'            => 'docs/%version%/',
+    'cache_dir'            => 'cache/%version%/',
     'template_dirs'        => array(
         __DIR__.'/themes/main/',
     ),
