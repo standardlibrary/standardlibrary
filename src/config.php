e6a266ac2e641c6a57d5d91ae2534eabe5af3e5a
unwarysheep <unwarysheep@icloud.com>
2018-01-26 16:14:26 +0000

    Add 1.0 branch

--- src/config.php
+++ src/config.php
@@ -14,7 +14,7 @@ $iterator = Finder::create()
 // generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
 $versions = GitVersionCollection::create($dir)
     ->addFromTags('v1.0.*')
-    ->add('1.0', '1.0 branch')
+    //->add('1.0', '1.0 branch')
     ->add('master', 'master branch')
 ;
 
