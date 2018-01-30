0bd2a06a880c98949a86f0fb545cf2d00846b0c1
unwarysheep <unwarysheep@icloud.com>
2018-01-30 15:18:22 +0000

    Add files

--- src/config.php
+++ src/config.php
@@ -12,20 +12,20 @@ $iterator = Finder::create()
 ;
 
 // generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
-$versions = GitVersionCollection::create($dir)
-    ->addFromTags('v1.0.*')
-    //->add('1.0', '1.0 branch')
-    ->add('master', 'master branch')
-;
+// $versions = GitVersionCollection::create($dir)
+//    ->addFromTags('v1.0.*')
+//    //->add('1.0', '1.0 branch')
+//    ->add('master', 'master branch')
+//;
 
 return new Sami($iterator, array(
     'theme'                => 'main',
     'title'                => 'Standard Library',
-    'versions'             => $versions,
+    //'versions'             => $versions,
     'build_dir'            => 'docs/%version%/',
     'cache_dir'            => 'cache/%version%/',
     'template_dirs'        => array(
         __DIR__.'/themes/main/',
     ),
-    'default_opened_level' => 2,
+    'default_opened_level' => 0,
 ));
